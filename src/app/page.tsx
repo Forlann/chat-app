66794479601d1dba8fdcb0137ac4c8a95a825854
Kaue <jkaueforlan@gmail.com>
2024-03-13 20:39:00 -0300

    Adicionand login.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello World!</p>
+      <p className="text-lg font-large">Hello World!</p>
       <Button variant="outline">Button</Button>
     </main>
   );
