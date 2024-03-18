ef5c3965225b0f9045ed1cf4ad814df1bcfde4ff
Kaue <jkaueforlan@gmail.com>
2024-03-18 20:29:26 -0300

    Desistanlando React Router e excluin main.jsx

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p className="text-lg font-large">Hello World!</p>
-      <Button variant="outline">Button</Button>
+      <Link href={'/'}>
+        <Button variant="outline">Button</Button>
+      </Link>
     </main>
   );
 }
