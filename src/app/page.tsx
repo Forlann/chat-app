bde3e993841799067c1285ed4d884447e7d8d3b0
Kaue <jkaueforlan@gmail.com>
2024-03-18 23:17:05 -0300

    Criado Chat_page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,18 @@
-import Image from "next/image"
-import './globals.css'
+'use client'
+
+import Image from "next/image";
 import { Button } from "@/components/ui/button"
-import { Icons } from "@/components/icons"
-// import { ChevronRightIcon } from "@radix-ui/react-icons"
-import { Input } from "@/components/ui/input"
-import {Avatar,AvatarFallback,AvatarImage,} from "@/components/ui/avatar"
+import Link from "next/link";
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+
+  const router = useRouter();
+
   return (
-    
-    <main className="bg-current text-foreground min-h-screen w-full">
-      <div>
-        {/*Topo Mensagem*/}
-        <div>
-          {/*Perfil*/}
-          <div className="border-b-2 px-1 py-1 pb-2 flex w-full items-start space-x-2 space-y-3">
-            <Avatar  className="ml-2 mt-2">
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <p className="text-white text-lg font-large">Igor Limaverde</p>
-          </div>
-          {/**/}
-        </div>
-
-        {/*Corpo Mensagem*/}
-        <div className="flex flex-col w-full">
-          {/*Texto Recebido*/}
-          <div className="flex flex-col items-start">
-            <span className="bg-gray-500 text-white px-2 py-4 ml-2 mt-2 mb-2 rounded-b-xl rounded-tl-xl"> 
-              How Can I Help?
-            </span>
-          </div>
-
-          {/*Texto Enviado*/}
-          <div className="flex flex-col items-end">
-            <span className="bg-blue-500 px-2 py-4 mr-2 mt-2 mb-2 rounded-b-xl rounded-tr-xl">
-              Good Place for Coffee.
-            </span>
-          </div>
-          {/*Input Mensagem*/}
-          <div className="border-t-2 px-1 py-2 flex items-center space-x-2 absolute bottom-0 w-full">
-            <Input  className="text-white" type="" placeholder="Type Message..." />
-            <Button variant="outline" size="icon"><Icons.arrow_rigt/></Button>
-          </div>
-        </div>
-
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className="text-lg font-large">Main Page</p>
+      <Button variant="outline" onClick={() => router.push('./login')}>Button</Button>
     </main>
   );
 }
