74d4a05abb14bcb5bc994087c32f85cbb6cbbfd0
Kauê.Forlan <jkaueforlan@gmail.com>
2024-04-15 16:28:30 -0300

    Correção de CSS para mensagens muito grandes

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,5 +1,6 @@
 import {Avatar, AvatarImage,} from "@/components/ui/avatar"
 import clsx from "clsx";
+import { wrap } from "module";
 
 interface MessageProps {
     content: string,
@@ -10,7 +11,7 @@ const Message: React.FC<MessageProps> = ({ content, myMessage }) => {
   const messageClass = myMessage ? "sended" : "received";
 
   return (
-    <div className={`${messageClass} flex mt-4 mb-4 rounded-b-xl text-white px-2 py-4 size-fit min-w-10 justify-center m-1`}>
+    <div style={{ overflowWrap: 'anywhere', maxWidth: '50%' }} className={`${messageClass} flex mt-4 mb-4 rounded-b-xl text-white px-2 py-4 size-fit min-w-10 justify-center m-1`} >
       <span>
         {content}
       </span>
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,26 +1,26 @@
-import express from 'express';
-import http from 'http';
-import { Server, Socket } from 'socket.io';
+import express from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-    credentials: true
-  }
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
-const port = 8080
+const port = 8080;
 
 // Armazena os IDs de socket associados aos usuários
 const users: { [key: string]: string } = {};
 
-io.on('connection', (socket: Socket) => {
-  console.log('Um cliente se conectou');
+io.on("connection", (socket: Socket) => {
+  console.log("Um cliente se conectou");
 
-  socket.on('disconnect', () => {
-    console.log('Um cliente se desconectou');
+  socket.on("disconnect", () => {
+    console.log("Um cliente se desconectou");
 
     // Remove o usuário desconectado da lista
     if (users[socket.id]) {
@@ -28,21 +28,23 @@ io.on('connection', (socket: Socket) => {
     }
   });
 
-  socket.on('set username', (username: string) => {
-    console.log('Username definido:', username);
+  socket.on("set username", (username: string) => {
+    console.log("Username definido:", username);
 
     // Associa o ID do socket com o nome de usuário
     users[socket.id] = username;
   });
 
-  socket.on('chat message', (messageObject: { content: string}) => {
-    
+  socket.on("chat message", (messageObject: { content: string }) => {
     // Envia a mensagem de volta apenas para o destinatário correto
     const senderUsername = users[socket.id];
     console.log(`Mensagem recebida: ${senderUsername}`, messageObject);
 
     if (senderUsername) {
-      socket.broadcast.emit('chat message', { content: messageObject.content, sender: senderUsername });
+      socket.broadcast.emit("chat message", {
+        content: messageObject.content,
+        sender: senderUsername,
+      });
     }
   });
 });
