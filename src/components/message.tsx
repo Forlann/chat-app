17e12f6466382899905cebc4aadce4953d43fb78
Kauê.Forlan <jkaueforlan@gmail.com>
2024-04-15 08:57:05 -0300

    Criado custom CSS pro componete de mensagem

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -7,13 +7,12 @@ interface MessageProps {
 }
 
 const Message: React.FC<MessageProps> = ({ content, myMessage }) => {
-  const messageClass = myMessage ? "enviado" : "recebido";
-  const bubbleClass = clsx(
-    `bg-${myMessage ? "blue" : "gray"}-500 items-${myMessage ? "end" : "start"} ml-${myMessage ? "auto" : "8px" }`);
+  const messageClass = myMessage ? "sended" : "received";
+  // const bubbleClass = clsx(`bg-${myMessage ? "blue" : "gray"}-500 items-${myMessage ? "end" : "start"} ml-${myMessage ? "auto" : "8px"}`);
 
   return (
-    <div className={`${messageClass} ${bubbleClass} flex mt-4 mb-4 rounded-b-xl text-white px-2 py-4 size-fit min-w-10 justify-center m-1`}>
-      <span className={bubbleClass}>
+    <div className={`${messageClass} flex mt-4 mb-4 rounded-b-xl text-white px-2 py-4 size-fit min-w-10 justify-center m-1`}>
+      <span>
         {content}
       </span>
     </div>
